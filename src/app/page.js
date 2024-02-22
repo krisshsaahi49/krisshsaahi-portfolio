e70aed2999e1c01a30190297234621974e86bb5a
Krishna Saahi Yavana <yavak01@pfw.edu>
2024-02-22 13:31:02 -0500

    added speed analytics

--- src/app/page.js
+++ src/app/page.js
@@ -5,12 +5,14 @@ import ProjectsSection from "./components/ProjectsSection";
 import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
 import AchievementsSection from "./components/AchievementsSection";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
+        <SpeedInsights/>
         <HeroSection />
         <AchievementsSection />
         <AboutSection />
